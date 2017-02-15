10ed5246c2629d2b560914fda3b7d3bbc12b9741
Маздайщик <a.v.konovalov87@mail.ru>
2017-02-15 11:13:25 +0300

    Переупорядочивание функций

--- task_1.py
+++ task_1.py
@@ -7,6 +7,17 @@ import os
 import preproc
 import sys
 
+def exec_preproc(input_name):
+    lines = preproc.preproc_file(input_name)
+    (result, stdout) = exec_function_from(lines, 'gcd', [3684468, 17354368])
+    return [repr(result)] + stdout
+
+def exec_function_from(code, funcname, args):
+    context = {'trace_args' : args}
+    code += '\ntrace_res = ' + funcname + '(*trace_args)'
+    stdout = exec_text(code, context)
+    return (context['trace_res'], stdout)
+
 def exec_text(text, context):
     with open('~output.tmp', 'w') as fout, \
          contextlib.redirect_stdout(fout):
@@ -15,17 +26,6 @@ def exec_text(text, context):
         stdout = [line.rstrip() for line in fin]
     os.remove('~output.tmp')
     return stdout
-
-def exec_function_from(code, funcname, args):
-    context = {'trace_args' : args}
-    code += '\ntrace_res = ' + funcname + '(*trace_args)'
-    stdout = exec_text(code, context)
-    return (context['trace_res'], stdout)
-
-def exec_preproc(input_name):
-    lines = preproc.preproc_file(input_name)
-    (result, stdout) = exec_function_from(lines, 'gcd', [3684468, 17354368])
-    return [repr(result)] + stdout
 
 if __name__=='__main__':
     if len (sys.argv) > 1:
